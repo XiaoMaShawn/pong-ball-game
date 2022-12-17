bf05f189492784b135303f12c2ddec43e4d35c43
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-12-17 13:22:07 -0500

    ball direction logic

--- Ball.js
+++ Ball.js
@@ -1,5 +1,46 @@
+const INITIAL_VELOCITY = .025;
+
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
+    this.reset();
+  }
+
+  //get the x,y value from style.css and parse it into a JS number that we can use; set the x,y to the value we give
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+  }
+
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value);
+  }
+
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+  }
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value);
+  }
+
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    this.direction = { x: 0 };
+    while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+      const heading = randomNumberBetween(0, 2 * Math.PI);
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+    }
+    console.log(this.direction);
+    this.velocity = INITIAL_VELOCITY;
+  }
+
+  update(delta) {
+    this.x = 5;
+    this.y = 15;
   }
 }
