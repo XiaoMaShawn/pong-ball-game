987c2715b98d7f10b1d04715df4c880a3bccb0d3
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-12-19 21:36:03 -0500

    player paddle

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 //update loop --> function --> check every piece of the website
 let lastTime;
@@ -8,10 +11,14 @@ function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
     //update code
-    ball.update(delta);
+    // ball.update(delta);
   }
   lastTime = time;
   window.requestAnimationFrame(update)
 };
+
+document.addEventListener("mousemove", e => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+})
 
 window.requestAnimationFrame(update)
